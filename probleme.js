285cae8f7b0dc22776e58d0988c2ef8f7ce17c39
Zo-en <e.matosin@hotmail.fr>
2023-11-11 18:04:00 +0100

    modif du probleme.js

--- probleme.js
+++ probleme.js
@@ -1,6 +1,6 @@
-function addition(a, b) {
-  return a + b;
+function division(a, b) {
+  return a / b;
 }
 
 // Utilisation de la fonction
-console.log(addition(5, "10"));
+console.log(division(10, 0));
